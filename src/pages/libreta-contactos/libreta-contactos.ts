7a3bfe3fdaec21a2e6d1a22600dc38b9a96adae6
Pablo Berciano <100363964@alumnos.uc3m.es>
2019-02-25 15:35:21 +0100

    Solucion

--- src/pages/libreta-contactos/libreta-contactos.ts
+++ src/pages/libreta-contactos/libreta-contactos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NuevoContactoPage } from '../nuevo-contacto/nuevo-contacto';
 
 /**
  * Generated class for the LibretaContactosPage page.
@@ -20,6 +21,10 @@ export class LibretaContactosPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LibretaContactosPage');
+  }
+
+  onLoadContactosPage(){
+    this.navCtrl.push(NuevoContactoPage);
   }
 
 }
